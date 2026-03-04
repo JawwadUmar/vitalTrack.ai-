e5431cf450ab38a855fad0bcaca64280c4dc261e
Jawwad Umar <jawwadumar99@gmail.com>
2026-03-05 02:13:33 +0530

    refactoring

---
+++ backend/models/ai.go
@@ -0,0 +1,75 @@
+package models
+
+import "fmt"
+
+type Message struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+type AIRequestBody struct {
+	Model       string    `json:"model"`
+	Input       []Message `json:"input"`
+	Temperature float64   `json:"temperature"`
+}
+
+type AIResponse struct {
+	Output []struct {
+		Type    string `json:"type"`
+		Role    string `json:"role"`
+		Content []struct {
+			Type string `json:"type"`
+			Text string `json:"text"`
+		} `json:"content"`
+	} `json:"output"`
+}
+
+const SystemPrompt string = `
+You are a medical report analysis assistant.
+I Should be able to convert returned JSON String to a struct using the standard library encoding/json
+Return ONLY valid JSON.
+Do NOT include markdown.
+Do NOT include explanations outside JSON.
+Do NOT provide diagnosis or medications.
+If data is missing, use null.
+If report_date is available, return it in the format DD-MM-YYYY
+
+Follow this exact JSON structure to produce JSON String:
+
+{
+  "report_metadata": {
+    "report_date": "",
+    "report_type": "",
+    "hospital_or_lab_name": ""
+  },
+  "metrics": [
+    {
+      "test_name": "",
+      "value": "",
+      "unit": "",
+      "reference_range": "",
+      "status": ""
+    }
+  ],
+  "abnormal_findings": [],
+  "simple_explanation": "",
+  "overall_risk_level": "",
+  "recommendations": {
+    "diet": [],
+    "lifestyle": []
+  },
+  "follow_up_suggestions": []
+}
+`
+
+func GetUserPrompt(ocrText string) string {
+	userPrompt := fmt.Sprintf(`
+Here is the extracted medical report text:
+
+%s
+
+Generate the structured JSON response following the schema.
+`, ocrText)
+
+	return userPrompt
+}
--- backend/service/ai.go
+++ backend/service/ai.go
@@ -8,132 +8,23 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"vita-track-ai/models"
 )
 
-type Message struct {
-	Role    string `json:"role"`
-	Content string `json:"content"`
-}
-
-type AIRequestBody struct {
-	Model       string    `json:"model"`
-	Input       []Message `json:"input"`
-	Temperature float64   `json:"temperature"`
-}
-
-type AIResponse struct {
-	Output []struct {
-		Type    string `json:"type"`
-		Role    string `json:"role"`
-		Content []struct {
-			Type string `json:"type"`
-			Text string `json:"text"`
-		} `json:"content"`
-	} `json:"output"`
-}
-
-type MedicalReport struct {
-	ReportMetadata struct {
-		ReportDate        string `json:"report_date"`
-		ReportType        string `json:"report_type"`
-		HospitalOrLabName string `json:"hospital_or_lab_name"`
-	} `json:"report_metadata"`
-
-	Metrics []struct {
-		TestName       string `json:"test_name"`
-		Value          string `json:"value"`
-		Unit           string `json:"unit"`
-		ReferenceRange string `json:"reference_range"`
-		Status         string `json:"status"`
-	} `json:"metrics"`
-
-	AbnormalFindings  []string `json:"abnormal_findings"`
-	SimpleExplanation string   `json:"simple_explanation"`
-	OverallRiskLevel  string   `json:"overall_risk_level"`
-
-	Recommendations struct {
-		Diet      []string `json:"diet"`
-		Lifestyle []string `json:"lifestyle"`
-	} `json:"recommendations"`
-
-	FollowUpSuggestions []string `json:"follow_up_suggestions"`
-}
-
-func ExtractMedicalReport(aiResp AIResponse) (*MedicalReport, error) {
-	for _, item := range aiResp.Output {
-		if item.Type == "message" && item.Role == "assistant" {
-			for _, content := range item.Content {
-				if content.Type == "output_text" {
-					var report MedicalReport
-					err := json.Unmarshal([]byte(strings.TrimSpace(content.Text)), &report)
-					if err != nil {
-						return nil, err
-					}
-					return &report, nil
-				}
-			}
-		}
-	}
-	return nil, fmt.Errorf("assistant JSON not found")
-}
-
-func AnalyzeMedicalReport(fileId string) (*MedicalReport, error) {
+func AnalyzeMedicalReport(fileId string) (*models.MedicalReport, error) {
 	ocrText, err := GenerateOCRText(fileId)
 	if err != nil {
-		return nil, fmt.Errorf("unable to load AWS config: %w", err)
+		return nil, fmt.Errorf("unable to get OCR text: %w", err)
 	}
 
-	systemPrompt := `
-You are a medical report analysis assistant.
-I Should be able to convert returned JSON String to a struct using the standard library encoding/json
-Return ONLY valid JSON.
-Do NOT include markdown.
-Do NOT include explanations outside JSON.
-Do NOT provide diagnosis or medications.
-If data is missing, use null.
+	userPrompt := models.GetUserPrompt(ocrText)
 
-Follow this exact JSON structure to produce JSON String:
-
-{
-  "report_metadata": {
-    "report_date": "",
-    "report_type": "",
-    "hospital_or_lab_name": ""
-  },
-  "metrics": [
-    {
-      "test_name": "",
-      "value": "",
-      "unit": "",
-      "reference_range": "",
-      "status": ""
-    }
-  ],
-  "abnormal_findings": [],
-  "simple_explanation": "",
-  "overall_risk_level": "",
-  "recommendations": {
-    "diet": [],
-    "lifestyle": []
-  },
-  "follow_up_suggestions": []
-}
-`
-
-	userPrompt := fmt.Sprintf(`
-Here is the extracted medical report text:
-
-%s
-
-Generate the structured JSON response following the schema.
-`, ocrText)
-
-	requestBody := AIRequestBody{
+	requestBody := models.AIRequestBody{
 		Model: "openai.gpt-oss-120b",
-		Input: []Message{
+		Input: []models.Message{
 			{
 				Role:    "system",
-				Content: systemPrompt,
+				Content: models.SystemPrompt,
 			},
 			{
 				Role:    "user",
@@ -170,13 +61,28 @@ Generate the structured JSON response following the schema.
 
 	body, _ := io.ReadAll(resp.Body)
 
-	var aiResp AIResponse
+	var aiResp models.AIResponse
 	err = json.Unmarshal(body, &aiResp)
 	if err != nil {
 		return nil, err
 	}
+	return extractMedicalReportFromAiResponse(aiResp)
+}
 
-	fmt.Print(aiResp)
-
-	return ExtractMedicalReport(aiResp)
+func extractMedicalReportFromAiResponse(aiResp models.AIResponse) (*models.MedicalReport, error) {
+	for _, item := range aiResp.Output {
+		if item.Type == "message" && item.Role == "assistant" {
+			for _, content := range item.Content {
+				if content.Type == "output_text" {
+					var report models.MedicalReport
+					err := json.Unmarshal([]byte(strings.TrimSpace(content.Text)), &report)
+					if err != nil {
+						return nil, err
+					}
+					return &report, nil
+				}
+			}
+		}
+	}
+	return nil, fmt.Errorf("assistant JSON not found")
 }
