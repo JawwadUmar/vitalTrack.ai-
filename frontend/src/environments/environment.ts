83e263fe6ccb82a3fbf664f4cc4a9f265c3a1be8
Jawwad Umar <jawwadumar99@gmail.com>
2026-04-29 13:34:15 +0530

    google login added

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'https://vitaltrack-ai.onrender.com/api/v1',
   emailVerificationEnabled: true,   //  OTP-Flow Set to true when DISABLE_EMAIL_FLOW=false on backend
+  googleClientId: '973447600517-6je9a56irb587joe1272ckju164steaa.apps.googleusercontent.com',                // Set this to your Google OAuth Web client ID to enable Google login
 };
