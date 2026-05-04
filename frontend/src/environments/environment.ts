4873269960d7783d3c7e15ca1a51efa99b12cff2
Harsh Jha <harshj92002@gmail.com>
2026-05-05 00:15:23 +0530

    Added

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8081/api/v1',
+  apiUrl: 'https://vitaltrack-ai.onrender.com/api/v1',
   emailVerificationEnabled: true,   //  OTP-Flow Set to true when DISABLE_EMAIL_FLOW=false on backend
   googleClientId: '885027957117-koo8e2lf498c0gs63f36jjru1jmig0f6.apps.googleusercontent.com',                // Set this to your Google OAuth Web client ID to enable Google login
 };
